b66e55aee9b72dfda92c297221905b90601e8c61
NLOG <sorisem4106@naver.com>
2017-09-28 20:22:51 +0900

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -384,8 +384,10 @@ Get-ChildItem "$dir\pmmp\plugins\DevTools\" -Filter *.phar |
 }
 
 New-ZipArchive "$dir\PocketMine\" "$dir\PocketMine.zip"
-New-ZipArchive "$dir\pmmp\src\" "$dir\PocketMine-Source.zip"
 echo "It will make zip..."
+start-sleep 10
+
+New-ZipArchive "$dir\pmmp\src\" "$dir\PocketMine-Source.zip"
 start-sleep 10
 
 echo " "
